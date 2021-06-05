a6ff26b91b3eb42818966ca3b19e2b7e1620ad4b
Beretta <xxfr3@DESKTOP-1S54KC2>
2021-06-05 16:34:30 -0500

    Added few more buttons and GUI shows toggles

--- VRC Object Sync Gravity Power Thing/Class1.cs
+++ VRC Object Sync Gravity Power Thing/Class1.cs
@@ -8,13 +8,14 @@ using VRCSDK2;
 
 [assembly: MelonInfo(typeof(Mod), "Faggot Gravity Gun Thing", "1", "Not Beretta", "https://github.com/cutielovesyou/VRC-Object-Sync-Gravity-Power-Thing")]
 [assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonPriority(MelonBase.MelonPriority.LOWEST)]
 
 public class Mod : MelonMod
 {
-    private bool Toggle = false;
-    private bool LaunchToggle = true;
-    private bool SmoothToggle = true;
-    private bool RotateWithHead = true;
+    public static bool Toggle = false;
+    public static bool LaunchToggle = true;
+    public static bool SmoothToggle = true;
+    public static bool RotateWithHead = true;
     private List<GameObject> GrabbedObjects = new List<GameObject>();
     private Dictionary<GameObject, GameObject> RaycastPointObjects = new Dictionary<GameObject, GameObject>();
     private GameObject SelectSphere;
@@ -24,15 +25,39 @@ public class Mod : MelonMod
     {
         MelonLogger.Msg("\n\n" +
             "===== CONTROLS =====\n\n" +
-            "[Return (Enter) + 0] To activate mod, left click for selecting and hold to do the cool stuff.\n" +
+            "[Return (Enter) + 0] To activate mod, right click for selecting and hold to do the cool stuff.\n" +
             "[Return (Enter) + 1] To toggle launch option (move direction after letting go of it)\n" +
             "[Return (Enter) + 2] To toggle smooth movement.\n" +
             "[Return (Enter) + 3] Emergency clear grabbed objects.\n" +
             "[Return (Enter) + 4] Reset ball size.\n" +
             "[Return (Enter) + 5] To toggle rotation with head.\n" +
             "[Return (Enter) + 6] Max ball size.\n" +
+            "[Return (Enter) + 7] Emergency respawn.\n" +
+            "[Return (Enter) + 8] Print all dynamic prefabs.\n" +
+            "[Return (Enter) + 9] Spawn all dynamic prefabs where you are looking.\n" +
             "[- or =] to change orb size.\n" +
             "Scroll wheel to drag in and out.\n\n");
+    }
+
+    public override void OnGUI()
+    {
+        short IndexY = 10;
+        foreach (var Toggle in typeof(Mod).GetFields())
+        {
+            if(Toggle.FieldType == typeof(bool))
+            {
+                if ((bool)Toggle.GetValue(null))
+                {
+                    GUI.color = Color.green;
+                }
+                else
+                {
+                    GUI.color = Color.red;
+                }
+                GUI.Label(new Rect(10,IndexY, 200,20), $"{Toggle.Name}: {(bool)Toggle.GetValue(null)}");
+                IndexY += 25;
+            }
+        }
     }
 
     public override void OnUpdate()
@@ -58,6 +83,7 @@ public class Mod : MelonMod
                 Color color = new Color(0, 1, 1, .3f);
                 material.color = color;
                 SelectSphere.GetComponent<Renderer>().material = material;
+                SelectSphere.SetActive(false);
             }
             if (Input.GetKey(KeyCode.Return))
             {
@@ -97,6 +123,46 @@ public class Mod : MelonMod
                 if (Input.GetKeyDown(KeyCode.Alpha6))
                 {
                     SelectSphereMultiplier = ushort.MaxValue;
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha7))
+                {
+                    VRCPlayer.field_Internal_Static_VRCPlayer_0.transform.position = UnityEngine.Object.FindObjectOfType<VRC.SDKBase.VRC_SceneDescriptor>().spawns.GetEnumerator().Current.position;
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha8))
+                {
+                    ushort Index = 1;
+                    foreach (var DP in UnityEngine.Object.FindObjectOfType<VRC.SDKBase.VRC_SceneDescriptor>().DynamicPrefabs)
+                    {
+                        MelonLogger.Msg($"{Index} - {DP.name}");
+                    }
+                }
+                if (Input.GetKey(KeyCode.Alpha9))
+                {
+                    GameObject gameObject = new GameObject();
+                    VRC.SDKBase.VRC_Trigger.TriggerEvent triggerEvent = new VRC.SDKBase.VRC_Trigger.TriggerEvent();
+                    VRC.SDKBase.VRC_EventHandler.VrcEvent vrcEvent = new VRC.SDKBase.VRC_EventHandler.VrcEvent();
+                    VRCSDK2.VRC_Trigger vrc_Trigger = gameObject.AddComponent<VRCSDK2.VRC_Trigger>();
+                    vrcEvent.EventType = VRC.SDKBase.VRC_EventHandler.VrcEventType.SpawnObject;
+                    vrc_Trigger.Triggers.Add(triggerEvent);
+                    triggerEvent.Events.Add(vrcEvent);
+                    triggerEvent.BroadcastType = VRC.SDKBase.VRC_EventHandler.VrcBroadcastType.Always;
+                    triggerEvent.TriggerType = VRC.SDKBase.VRC_Trigger.TriggerType.OnInteract;
+                    vrc_Trigger.Interact();
+                    foreach (var DP in UnityEngine.Object.FindObjectOfType<VRC.SDKBase.VRC_SceneDescriptor>().DynamicPrefabs)
+                    {
+                        var _DP = GameObject.Instantiate(DP.gameObject);
+                        if (Physics.Raycast(Camera.current.transform.position, Camera.current.transform.forward, out var Hit, short.MaxValue))
+                        {
+                            vrcEvent.ParameterString = DP.name;
+                            gameObject.transform.position = Hit.point;
+                            vrcEvent.ParameterObjects = new UnhollowerBaseLib.Il2CppReferenceArray<GameObject>(new GameObject[] { gameObject });
+                            vrc_Trigger.Interact();
+                        }
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha7))
+                {
+                    VRCPlayer.field_Internal_Static_VRCPlayer_0.transform.position = UnityEngine.Object.FindObjectOfType<VRC.SDKBase.VRC_SceneDescriptor>().spawns.GetEnumerator().Current.position;
                 }
                 if (Print)
                 {
